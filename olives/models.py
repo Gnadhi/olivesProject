830b9dacbc0aadd3b3dcde874c5071c8034e4024
Gnadhi <2375417g@student.gla.ac.uk>
2020-03-05 00:34:14 +0000

    Added Slug feild to the menus model

--- olives/models.py
+++ olives/models.py
@@ -1,18 +1,26 @@
 import os
 
 from django.db import models
+from django.utils.text import slugify
 
 
 class Menu(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=250)
+    slug = models.CharField(max_length=50, null=True)
     # Note in the database only the stores the url to these items and not the items themselves
     # Note it will store it in the MEDIA/ folder
     image = models.ImageField(upload_to=os.path.join("menus", "images"))
     file = models.FileField(upload_to=os.path.join("menus", "files"))
 
+    def save(self, *args, **kwargs):  # save method is always called when creating or updating an instance of Django
+        # model
+        self.slug = slugify(self.name)  # slugify creates a url safe name by removing the spaces for dashes
+        super(Menu, self).save(*args, **kwargs)
+
     class Meta:
         verbose_name_plural = 'Menu'
+
 
 class Dishes(models.Model):
     name = models.CharField(max_length=50)
@@ -20,7 +28,7 @@ class Dishes(models.Model):
     dislike = models.IntegerField(default=0)
 
     class Meta:
-        verbose_name_plural='Dishes'
+        verbose_name_plural = 'Dishes'
 
     def __str__(self):
         return self.name
--- populate_dishes.py
+++ populate_dishes.py
@@ -1,29 +1,33 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','olivesProject.settings') 
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'olivesProject.settings')
 
 import django
+
 django.setup()
 from olives.models import Dishes
 
+
 def populate():
     dishes = [
-            {'name':'Pav Bhaji','likes':100,'dislike':3},
-            {'name':'Pani Puri','likes':300,'dislike':10},
-            {'name':'Masala Dosa','likes':153,'dislike':32},
-            {'name':'Paneer Butter Masala','likes':654,'dislike':12},
-            {'name':'Kadhai Paneer','likes':123,'dislike':36},
-            {'name':'Tadka Dal','likes':459,'dislike':45},
-            {'name':'Dal Makhani','likes':157,'dislike':100} ]
-    for i in range(0,len(dishes)):
-        add_dishes(dishes[i]['name'],dishes[i]['likes'],dishes[i]['dislike'])
+        {'name': 'Pav Bhaji', 'likes': 100, 'dislike': 3},
+        {'name': 'Pani Puri', 'likes': 300, 'dislike': 10},
+        {'name': 'Masala Dosa', 'likes': 153, 'dislike': 32},
+        {'name': 'Paneer Butter Masala', 'likes': 654, 'dislike': 12},
+        {'name': 'Kadhai Paneer', 'likes': 123, 'dislike': 36},
+        {'name': 'Tadka Dal', 'likes': 459, 'dislike': 45},
+        {'name': 'Dal Makhani', 'likes': 157, 'dislike': 100}]
+    for i in range(0, len(dishes)):
+        add_dishes(dishes[i]['name'], dishes[i]['likes'], dishes[i]['dislike'])
     for d in Dishes.objects.all():
         print(f'- {d}')
 
 
-def add_dishes(name,likes,dislike):
-    d = Dishes.objects.get_or_create(name=name,likes=likes,dislike=dislike)[0]
+def add_dishes(name, likes, dislike):
+    d = Dishes.objects.get_or_create(name=name, likes=likes, dislike=dislike)[0]
     d.save()
     return d
+
 
 if __name__ == '__main__':
     print('Starting Olive Dish Population Script...')
