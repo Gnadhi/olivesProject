135432f7e38516b7100a671a22e37e78ffb1005f
Karthik <dubevedant@gmail.com>
2020-03-05 17:54:10 +0000

    Working Version

--- olives/urls.py
+++ olives/urls.py
@@ -5,14 +5,11 @@ from olives import views
 app_name = "olives"
 
 urlpatterns = [
-<<<<<<<
     path("", views.index, name="index"),  # The index is the home page of the website
     path("dish-review/", views.dishReview, name="dishReview"),
-    path("staff-sign-up/", views.staffSignUp, name="staffSignUp")
-=======
+    path("staff-sign-up/", views.staffSignUp, name="staffSignUp"),
     path('admin/', admin.site.urls, name = "admin"),
     path('index/', views.index, name = 'index'),  # The index is the home page of the website
     path("dish-review/", views.dishReview, name = "dishReview"),
     
->>>>>>>
 ]
