2562b1f7e537783ee5bd684aedcfb9f69e2792cf
Gnadhi <2375417g@student.gla.ac.uk>
2020-03-05 01:11:08 +0000

    Finnished the following tables: Order, Custoomer and Booking. Changed names of Dihes model to Dish

--- olives/models.py
+++ olives/models.py
@@ -1,7 +1,8 @@
-import os
+import os  # Needed to use the os.path function
 
-from django.db import models
-from django.utils.text import slugify
+from django.db import models  # Needed to uses django models
+from django.template.defaultfilters import slugify  # Needed to uses the slugify function
+from django.contrib.auth.models import User  # Needed to create a 1 to 1 link with the django built in User model
 
 
 class Menu(models.Model):
@@ -19,12 +20,12 @@ class Menu(models.Model):
         super(Menu, self).save(*args, **kwargs)
 
     class Meta:
-        verbose_name_plural = 'Menu'
+        verbose_name_plural = 'Menus'
 
 
-class Dishes(models.Model):
-    name = models.CharField(max_length=50)
-    likes = models.IntegerField(default=0)
+class Dish(models.Model):
+    name = models.CharField(max_length=50)  # The name of the dish
+    likes = models.IntegerField(default=0)  # The number of likes the dishes have
     dislike = models.IntegerField(default=0)
 
     class Meta:
@@ -32,3 +33,42 @@ class Dishes(models.Model):
 
     def __str__(self):
         return self.name
+
+
+# Order has a many to many relationship with dishes
+class Order(models.Model):
+    orderId = models.CharField(max_length=64)
+    dishes = models.ManyToManyField(Dish)  # Creates the many to many link with the dishes table
+
+    class Meta:
+        verbose_name_plural = 'Orders'
+
+    def __str__(self):
+        return self.orderId
+
+
+# Order has many to 1 relationship with Customer
+class Customer(models.Model):
+    # This line is required as it links user profile to a user model instance.
+    customer = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # The additional attributes we wish to include
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)  # One customer can have many orders
+
+    def __str__(self):
+        return self.customer.username
+
+
+# Booking has 1 to 1 relationship with Customer
+class Booking(models.Model):
+    bookingId = models.CharField(max_length=64, unique=True)
+    phone = models.CharField(max_length=15)
+    noOfPeople = models.IntegerField()
+    date = models.DateField()
+    time = models.TimeField()
+
+    class Meta:
+        verbose_name_plural = 'Bookings'
+
+    def __str__(self):
+        return self.bookingId
