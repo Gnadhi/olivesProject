9f8b902e1fba43d6ea8a621c0c9bb5918921ea56
Umar Ibrahim <2366908I@student.gla.ac.uk>
2020-03-10 00:34:47 +0000

    Added remaining static pages to nav. To work on JS now and will style login/reg page

--- olives/urls.py
+++ olives/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
 from olives import views
 
+from django.contrib import admin
+
+
 app_name = "olives"
 
 urlpatterns = [
-    path("", views.index, name="index")  # The index is the home page of the website
+    path("index/", views.index, name="index"), # The index is the home page of the website
+    path('about/', views.about, name='about'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('special-events/', views.specialevents, name='special-events'),
+    path("dish-review/", views.dishReview, name="dishReview"),
+    path("staff-sign-up/", views.staffSignUp, name="staffSignUp"),
+    path('admin/', admin.site.urls, name="admin"),
+    path("add_dish/", views.add_dish, name="add_dish"),
+    path("booking/", views.make_booking, name="booking"),
+    path("delete_dish/", views.delete_dish, name="delete_dish"),
 ]
--- olives/views.py
+++ olives/views.py
@@ -1,8 +1,111 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseRedirect
+from olives.forms import StaffSignUpForm, BookingForm
+from olives.models import Dish
+from olives.forms import DishForm, DishDeleteForm
+from django.contrib import messages
+from django.views import View
+from django.contrib.auth import authenticate, login
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect,reverse
 
 def index(request):
     # PLACEHOLDER !!!
-    response = render(request, "base.html")
+    response = render(request, "olives/index.html")
     return response
+
+def about(request):
+
+	return render(request, "olives/about.html")
+
+
+def gallery(request):
+	return render(request, "olives/gallery.html")
+
+def specialevents(request):
+	return render(request, "olives/special-events.html")
+
+
+def dishReview(request):
+    dishList = Dish.objects.order_by('-likes')[:5]
+    allDishList = Dish.objects.all()
+    context_dict = {}
+    context_dict['boldmessage'] = 'The top five dishes of Olives & Pesto:'
+    context_dict['TopDishes'] = dishList
+    context_dict['AllDishes'] = allDishList
+    response = render(request, "olives/reviewDishes.html", context=context_dict)
+    return response
+
+
+def add_dish(request):
+    form = DishForm()
+
+    if request.method == 'POST':
+        form = DishForm(request.POST)
+
+        if form.is_valid():
+            if (Dish.objects.filter(name=form.cleaned_data['name']).exists() == False):
+                form.save(commit=True)
+                return redirect('/olives/add_dish')
+            else:
+                messages.warning(request, 'The dish already exists!')
+                render(request, 'olives/add_dish.html', {'form': form})
+        else:
+            render(request, 'olives/add_dish.html', {'form':form})
+            print(form.errors)
+    return render(request, 'olives/add_dish.html', {'form': form})
+
+
+def delete_dish(request):
+    
+    if request.method == 'POST':
+        form = DishDeleteForm(request.POST)
+        if form.is_valid():
+            dishID = form.cleaned_data.get('dishDelete')
+
+            dish  = Dish.objects.filter(id=dishID).first().delete()
+            print(dish)
+            return HttpResponseRedirect(reverse('olives:delete_dish'))
+        else:
+            print(form.errors)
+    return render(request,'olives/delete_dish.html',{'form':form})
+
+
+def staffSignUp(request):
+    if request.method == 'POST':
+        form = StaffSignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            # login(request, user)
+            return redirect("olives:index")
+    else:
+        form = StaffSignUpForm()
+    return render(request, 'olives/staffRegister.html', {'form': form})
+
+
+def make_booking(request):
+    form = BookingForm()
+
+    if request.method == "POST":
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            # Sets up Email details
+            mail_subject = "Booking Confirmation"
+            mail_body = "Name: " + form.cleaned_data.get("name") + "\n" \
+                        + "Phone: " + form.cleaned_data.get("phone") + "\n" \
+                        + "Number of People: " + str(form.cleaned_data.get("noOfPeople")) + "\n" \
+                        + "Date: " + str(form.cleaned_data.get("date")) + "\n" \
+                        + "Time: " + str(form.cleaned_data.get("time")) + "\n"
+            mail_sender = "Booking@olivesandpesto.com"
+            mail_sendAddress = form.cleaned_data.get("email")
+            # Send Mail takes 4  required parameters - Subject Line, Message Body, Sender Address, Send Address
+            send_mail(mail_subject, mail_body, mail_sender, [mail_sendAddress], fail_silently=False, )
+            return redirect("olives:index")
+        else:
+            print(form.errors)
+
+    return render(request, "olives/booking.html", {'form': form})
