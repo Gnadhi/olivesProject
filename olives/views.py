ff08c1d13b01acced3fba77c83af2ebfab6da55a
Umar Ibrahim <2366908I@student.gla.ac.uk>
2020-03-11 01:32:09 +0000

    Added images and animation. Need to resolve iteration issue.

--- olives/views.py
+++ olives/views.py
@@ -10,104 +10,114 @@ from django.shortcuts import render, redirect, reverse
 
 
 def index(request):
-    return render(request, "olives/index.html")
+	return render(request, "olives/index.html")
 
 
 def about(request):
-    return render(request, "olives/about.html")
+	return render(request, "olives/about.html")
 
 
 def gallery(request):
-    return render(request, "olives/gallery.html")
+	imageID = ['myImg', 'myImg2', 'myImg3', 'myImg4', 'myImg5', 'myImg6']
+	modalID = ['img01', 'img02', 'img03','img04','img05','img06']
+	imageURL = ['../../static/images/burger.jpg', '../../static/images/cakes.jpg',
+	'../../static/images/chicken.jpg', '../../static/images/snapper.jpg',
+	'../../static/images/spaghetti.jpg','../../static/images/steak.jpg']
+	captions = ['Burger and chips', 'Minced cake', 'Grilled chicken', 'Red snapper', 'Spaghetti meatballs',
+	'Steak']
+
+	images = zip(imageID, modalID, imageURL, captions) 
+
+	return render(request, "olives/gallery.html", {'images':images})
 
 
 def specialEvents(request):
-    return render(request, "olives/special-events.html")
+	return render(request, "olives/special-events.html")
 
 
 def dishReview(request):
-    dishList = Dish.objects.order_by('-likes')[:5]
-    allDishList = Dish.objects.all()
-    context_dict = {}
-    context_dict['boldmessage'] = 'The top five dishes of Olives & Pesto:'
-    context_dict['TopDishes'] = dishList
-    context_dict['AllDishes'] = allDishList
-    response = render(request, "olives/reviewDishes.html", context=context_dict)
-    return response
+	dishList = Dish.objects.order_by('-likes')[:5]
+	allDishList = Dish.objects.all()
+	context_dict = {}
+	context_dict['boldmessage'] = 'The top five dishes of Olives & Pesto:'
+	context_dict['TopDishes'] = dishList
+	context_dict['AllDishes'] = allDishList
+	response = render(request, "olives/reviewDishes.html", context=context_dict)
+	return response
 
 
 def add_dish(request):
-    form = DishForm()
+	form = DishForm()
 
-    if request.method == 'POST':
-        form = DishForm(request.POST)
+	if request.method == 'POST':
+		form = DishForm(request.POST)
 
-        if form.is_valid():
-            if (Dish.objects.filter(name=form.cleaned_data['name']).exists() == False):
-                form.save(commit=True)
-                return redirect('/olives/add_dish')
-            else:
-                messages.warning(request, 'The dish already exists!')
-                render(request, 'olives/add_dish.html', {'form': form})
-        else:
-            render(request, 'olives/add_dish.html', {'form': form})
-            print(form.errors)
-    return render(request, 'olives/add_dish.html', {'form': form})
+		if form.is_valid():
+			if (Dish.objects.filter(name=form.cleaned_data['name']).exists() == False):
+				form.save(commit=True)
+				return redirect('/olives/add_dish')
+			else:
+				messages.warning(request, 'The dish already exists!')
+				render(request, 'olives/add_dish.html', {'form': form})
+		else:
+			render(request, 'olives/add_dish.html', {'form': form})
+			print(form.errors)
+	return render(request, 'olives/add_dish.html', {'form': form})
 
 
 def delete_dish(request):
-    if request.method == 'POST':
-        form = DishDeleteForm(request.POST)
-        if form.is_valid():
-            dishID = form.cleaned_data.get('dishDelete')
-            dish = Dish.objects.filter(id=dishID).first().delete()
-            return HttpResponseRedirect(reverse('olives:delete_dish'))
-        else:
-            print(form.errors)
-    else:
-        form = DishDeleteForm()
-    return render(request, 'olives/delete_dish.html', {'form': form})
+	if request.method == 'POST':
+		form = DishDeleteForm(request.POST)
+		if form.is_valid():
+			dishID = form.cleaned_data.get('dishDelete')
+			dish = Dish.objects.filter(id=dishID).first().delete()
+			return HttpResponseRedirect(reverse('olives:delete_dish'))
+		else:
+			print(form.errors)
+	else:
+		form = DishDeleteForm()
+	return render(request, 'olives/delete_dish.html', {'form': form})
 
 
 def staffSignUp(request):
-    if request.method == 'POST':
-        form = StaffSignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            # login(request, user)
-            return redirect("olives:index")
-    else:
-        form = StaffSignUpForm()
-    return render(request, 'olives/staffRegister.html', {'form': form})
+	if request.method == 'POST':
+		form = StaffSignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			raw_password = form.cleaned_data.get('password1')
+			user = authenticate(username=username, password=raw_password)
+			# login(request, user)
+			return redirect("olives:index")
+	else:
+		form = StaffSignUpForm()
+	return render(request, 'olives/staffRegister.html', {'form': form})
 
 
 def make_booking(request):
-    form = BookingForm()
+	form = BookingForm()
 
-    if request.method == "POST":
-        form = BookingForm(request.POST)
-        if form.is_valid():
-            form.save(commit=True)
-            # Sets up Email details
-            mail_subject = "Booking Request Received"
-            mail_body = "A booking request has been received with the following details a confirmation email will be " \
-                        "sent shortly. " + "\n" \
-                        + "Name: " + form.cleaned_data.get("name") + "\n" \
-                        + "Phone: " + form.cleaned_data.get("phone") + "\n" \
-                        + "Number of People: " + str(form.cleaned_data.get("noOfPeople")) + "\n" \
-                        + "Date: " + str(form.cleaned_data.get("date")) + "\n" \
-                        + "Time: " + str(form.cleaned_data.get("time")) + "\n"
-            mail_sender = "Booking@olivesandpesto.com"
-            mail_sendAddress = form.cleaned_data.get("email")
-            # Send Mail takes 4  required parameters - Subject Line, Message Body, Sender Address, Send Address
-            send_mail(mail_subject, mail_body, mail_sender, [mail_sendAddress], fail_silently=False, )
-            return redirect("olives:index")
-        else:
-            print(form.errors)
+	if request.method == "POST":
+		form = BookingForm(request.POST)
+		if form.is_valid():
+			form.save(commit=True)
+			# Sets up Email details
+			mail_subject = "Booking Request Received"
+			mail_body = "A booking request has been received with the following details a confirmation email will be " \
+						"sent shortly. " + "\n" \
+						+ "Name: " + form.cleaned_data.get("name") + "\n" \
+						+ "Phone: " + form.cleaned_data.get("phone") + "\n" \
+						+ "Number of People: " + str(form.cleaned_data.get("noOfPeople")) + "\n" \
+						+ "Date: " + str(form.cleaned_data.get("date")) + "\n" \
+						+ "Time: " + str(form.cleaned_data.get("time")) + "\n"
+			mail_sender = "Booking@olivesandpesto.com"
+			mail_sendAddress = form.cleaned_data.get("email")
+			# Send Mail takes 4  required parameters - Subject Line, Message Body, Sender Address, Send Address
+			send_mail(mail_subject, mail_body, mail_sender, [mail_sendAddress], fail_silently=False, )
+			return redirect("olives:index")
+		else:
+			print(form.errors)
 
-    return render(request, "olives/booking.html", {'form': form})
+	return render(request, "olives/booking.html", {'form': form})
 
 
